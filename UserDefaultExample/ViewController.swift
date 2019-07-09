f00850bf26d8dbf35c3949ab7acdb5ffd1b3e8c2
iptkrisna <krisnamath9@gmail.com>
2019-07-09 15:16:12 +0700

    check it out

--- UserDefaultExample/ViewController.swift
+++ UserDefaultExample/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var wordTF: UITextField!
+    @IBOutlet weak var dataLabel: UILabel!
+    let userDef = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        dataLabel.text = userDef.string(forKey: "word")
+        
     }
 
-
+    @IBAction func saveButton(_ sender: UIButton) {
+        userDef.set(wordTF.text, forKey: "word")
+        dataLabel.text = userDef.string(forKey: "word")
+        
+        print(NSSearchPathForDirectoriesInDomains(.libraryDirectory, .userDomainMask, true))
+        
+    }
+    
 }
 
